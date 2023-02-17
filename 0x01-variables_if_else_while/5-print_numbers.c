2377cf71ab6a744b0eccdd9f3b855aee408365ed
Bakare Aminat Oluwatoyosi <aminatbkr@gmail.com>
2023-02-17 07:51:00 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+i#include <stdio.h>
+
+/**
+ *main - print 0123456789
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%i", n);
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}

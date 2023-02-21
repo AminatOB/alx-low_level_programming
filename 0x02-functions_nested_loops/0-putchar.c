778cb85bcacdedda09fbcc05277b1ec3953833af
Bakare Aminat Oluwatoyosi <aminatbkr@gmail.com>
2023-02-21 03:08:53 -0800

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "_putchar.h"
+
+/**
+ * main - print _putchar, followed by new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}

e5ab121de19baafe44f809341ee5c7023d87041c
Bakare Aminat Oluwatoyosi <aminatbkr@gmail.com>
2023-02-23 07:42:47 -0800

    main

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,34 @@
+#ifdef _MAIN_H_
+#define _MAIN_H_
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+void positive_or_negative(int i);
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void);
+
+/**
+* main - prints the largest of 3 integers
+* Return: 0
+*/
+
+int largest_number(int a, int b, int c);
+
+
+/**
+* main - takes a date and prints how many days are left in the year, taking
+* leap years into account
+* Return: 0
+*/
+
+int convert_day(int month, int day)
+
+#endif

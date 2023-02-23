b7a9a2d1d59fa063b837dfb4b617f50bbc21205c
Bakare Aminat Oluwatoyosi <aminatbkr@gmail.com>
2023-02-23 07:47:43 -0800

    mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%i is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%i is zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%i is negative\n", n);
+	}
+
+	return (0);
+}

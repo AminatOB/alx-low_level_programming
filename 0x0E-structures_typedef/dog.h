1544467d8861ddf6378b8d0d0980e86f32d59597
Bakare Aminat Oluwatoyosi <aminatbkr@gmail.com>
2023-03-20 09:42:29 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef DOG_H
+#define DOG_H
 
 /**
   * struct dog - Dog attributes
@@ -25,4 +25,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif

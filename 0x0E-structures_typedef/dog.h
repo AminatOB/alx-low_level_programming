9a26ae2e8bde9829a5104c7186f8b02fe4321ee2
Bakare Aminat Oluwatoyosi <aminatbkr@gmail.com>
2023-03-20 09:28:50 -0700

    poppY

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,5 +2,14 @@
 #define _HEADER_
 #define name char *
 #define age float
-#define owner char *
+#define owner char
+/**
+ * dog_t - Typedef for dog structure
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif

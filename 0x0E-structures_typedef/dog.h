9a85409693dcbf433dd6cffdb795e21b58be9f8d
Bakare Aminat Oluwatoyosi <aminatbkr@gmail.com>
2023-03-20 09:31:15 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,21 @@
 #ifndef _HEADER_
 #define _HEADER_
-#define name char *
-#define age float
-#define owner char
+
+/**
+  * struct dog - Dog attributes
+  * @name: The name of the dog.
+  * @age: The age of the dog.
+  * @owner: The owner of the dog.
+  *
+  * Description: The attributes of a dog.
+  */
+struct dog
+{
+	name char *;
+	age float;
+	owner char *;
+};
+
 /**
  * dog_t - Typedef for dog structure
  */

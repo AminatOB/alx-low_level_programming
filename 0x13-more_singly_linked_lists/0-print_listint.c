c49b0a4778f9aac9ecfe57201aafc92909acd375
Bakare Aminat Oluwatoyosi <aminatbkr@gmail.com>
2023-04-05 02:00:01 -0700

    first commiT

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
-/** 
- * print_listint: print the elements of a linked list.
+/**
+ * print_listint: to print the elements of a linked list.
  * @h: the linked list of listint_t to print.
- *
  * Return: the mumber of nodes (count).
  */
+
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
